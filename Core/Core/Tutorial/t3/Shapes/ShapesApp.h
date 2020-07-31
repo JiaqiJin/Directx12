3b1b30b15f92f8d5fe05a9285ce60a017e472491
JiaqiJin96 <fdsam@hotmail.com>
2020-07-31 19:12:16 +0200

    shapes class start

---
+++ Core/Core/Tutorial/t3/Shapes/ShapesApp.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "../Common/d3dApp.h"
+#include "../Common/MathHelper.h"
+#include "../Common/UploadBuffer.h"
+#include "../Common/GeometryGenerator.h"
+
+using Microsoft::WRL::ComPtr;
+using namespace DirectX;
+using namespace DirectX::PackedVector;
+
+/*
+Lightweight structure to store parameters to drae shape
+This will vary from app-to-app
+*/
+
+class ShapesApp : public D3DApp
+{
+
+};
+
+
