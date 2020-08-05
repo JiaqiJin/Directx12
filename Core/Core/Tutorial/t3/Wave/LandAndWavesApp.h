c5895bc0b8b2ec5f3387ef0090bd99e0c3a04e45
JiaqiJin96 <fdsam@hotmail.com>
2020-08-05 18:45:49 +0200

    wave

---
+++ Core/Core/Tutorial/t3/Wave/LandAndWavesApp.h
@@ -0,0 +1,155 @@
+#pragma once
+
+
+#include "../FrameResource.h"
+#include "../Common/d3dApp.h"
+#include "../Common/GeometryGenerator.h"
+#include "../Common/MathHelper.h"
+
+using Microsoft::WRL::ComPtr;
+using namespace DirectX;
+using namespace DirectX::PackedVector;
+
+const int gNumFrameResources = 3;
+
+struct RenderItem
+{
+	RenderItem() = default;
+
+	XMFLOAT4X4 World = MathHelper::Identity4x4();
+
+	int NumFramesDirty = gNumFrameResources;
+
+	//Index into GPU constant buffer
+	UINT ObjCBIndex = -1;
+
+	MeshGeometry* Geo = nullptr;
+
+	// DrawIndexedInstanced parameters.
+	UINT IndexCount = 0;
+	UINT StartIndexLocation = 0;
+	int BaseVertexLocation = 0;
+};
+
+class LandAndWavesApp : public D3DApp
+{
+public:
+	LandAndWavesApp(HINSTANCE hInstance);
+	LandAndWavesApp(const LandAndWavesApp& rhs) = delete;
+	LandAndWavesApp& operator=(const LandAndWavesApp& rhs) = delete;
+	~LandAndWavesApp();
+
+	virtual bool Initialize()override;
+
+private:
+	virtual void OnResize()override;
+	virtual void Update(const GameTimer& gt)override;
+	virtual void Draw(const GameTimer& gt)override;
+
+	virtual void OnMouseDown(WPARAM btnState, int x, int y)override;
+	virtual void OnMouseUp(WPARAM btnState, int x, int y)override;
+	virtual void OnMouseMove(WPARAM btnState, int x, int y)override;
+
+public:
+
+private:
+
+	bool mIsWireframe = false;
+
+	float mTheta = 1.5f * XM_PI;
+	float mPhi = XM_PIDIV2 - 0.1f;
+	float mRadius = 50.0f;
+
+	float mSunTheta = 1.25f * XM_PI;
+	float mSunPhi = XM_PIDIV4;
+
+	POINT mLastMousePos;
+};
+
+LandAndWavesApp::LandAndWavesApp(HINSTANCE hInstance)
+	: D3DApp(hInstance)
+{
+}
+
+LandAndWavesApp::~LandAndWavesApp()
+{
+	if (md3dDevice != nullptr)
+		FlushCommandQueue();
+}
+
+bool LandAndWavesApp::Initialize()
+{
+	if (!D3DApp::Initialize())
+		return false;
+
+	// Reset the command list to prep for initialization commands.
+	ThrowIfFailed(mCommandList->Reset(mDirectCmdListAlloc.Get(), nullptr));
+
+	
+
+	return true;
+}
+
+void LandAndWavesApp::OnResize()
+{
+	D3DApp::OnResize();
+
+	
+}
+
+void LandAndWavesApp::Update(const GameTimer& gt)
+{
+	
+}
+
+void LandAndWavesApp::Draw(const GameTimer& gt)
+{
+	
+}
+
+void LandAndWavesApp::OnMouseDown(WPARAM btnState, int x, int y)
+{
+	mLastMousePos.x = x;
+	mLastMousePos.y = y;
+
+	SetCapture(mhMainWnd);
+}
+
+void LandAndWavesApp::OnMouseUp(WPARAM btnState, int x, int y)
+{
+	ReleaseCapture();
+}
+
+void LandAndWavesApp::OnMouseMove(WPARAM btnState, int x, int y)
+{
+	if ((btnState & MK_LBUTTON) != 0)
+	{
+		// Make each pixel correspond to a quarter of a degree.
+		float dx = XMConvertToRadians(0.25f * static_cast<float>(x - mLastMousePos.x));
+		float dy = XMConvertToRadians(0.25f * static_cast<float>(y - mLastMousePos.y));
+
+		// Update angles based on input to orbit camera around box.
+		mTheta += dx;
+		mPhi += dy;
+
+		// Restrict the angle mPhi.
+		mPhi = MathHelper::Clamp(mPhi, 0.1f, MathHelper::Pi - 0.1f);
+	}
+	else if ((btnState & MK_RBUTTON) != 0)
+	{
+		// Make each pixel correspond to 0.2 unit in the scene.
+		float dx = 0.2f * static_cast<float>(x - mLastMousePos.x);
+		float dy = 0.2f * static_cast<float>(y - mLastMousePos.y);
+
+		// Update the camera radius based on input.
+		mRadius += dx - dy;
+
+		// Restrict the radius.
+		mRadius = MathHelper::Clamp(mRadius, 5.0f, 150.0f);
+	}
+
+	mLastMousePos.x = x;
+	mLastMousePos.y = y;
+}
+
+
